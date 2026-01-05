acec7a31763576bfe0a2b641176589e3fd8ad160
codefixer2 <tushargkd230@gmail.com>
2026-01-05 15:10:37 +0530

    updated

--- FRONTEND/src/components/dashboard/overview.jsx
+++ FRONTEND/src/components/dashboard/overview.jsx
@@ -1,2 +1,6 @@
 import React from 'react' ;
 import { DollarSign , Users, Breifcase, TrendingUp } from 'Lucide-react';
+import KPICard from '../Cards/KPICards';
+import PerformanceChart from '../Charts/PerformanceChart';
+import RevenueChart from '../Charts/RevenueChart';
+import RevenueChart from '../Charts/RevenueChart';
