a6ec05c82a526cb5dde079945870db3eb32ebd30
codefixer2 <tushargkd230@gmail.com>
2026-01-07 00:02:44 +0530

    Upadated js in folder

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,8 +1,13 @@
-import React,{UseState} from 'react';
+import React,{useState, UseState} from 'react';
 import Overview from './components/Dashboard/Overview';
 import Predictions from './components/Dashboard/Predications';
 import Resources from './components/Dashboard/Resources';
 
 function App() {
   //This kepps track of which tab is currently open 
+  const [activeTab, setActiveTab] = useState('Overview');
+
+  return(
+    <div className="Min-h-screen bg-gradient-to-br from-slate-50 to-100"
+  )
 }
