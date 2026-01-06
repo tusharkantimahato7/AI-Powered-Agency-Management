7ba2819d17538fb4e085d50be487474ab62557f9
codefixer2 <tushargkd230@gmail.com>
2026-01-06 23:47:44 +0530

    upadted imort React jsx

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,8 @@
+import React,{UseState} from 'react';
+import Overview from './components/Dashboard/Overview';
+import Predictions from './components/Dashboard/Predications';
+import Resources from './components/Dashboard/Resources';
+
+function App() {
+  //This kepps track of which tab is currently open 
+}
