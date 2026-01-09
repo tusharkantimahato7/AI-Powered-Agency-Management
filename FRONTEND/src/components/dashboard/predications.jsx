7e17a37a3ba5b9c65cd9650b11ab0b92fccd5f8e
codefixer2 <tushargkd230@gmail.com>
2026-01-09 23:44:22 +0530

    Updated Prediction on ml

---
+++ FRONTEND/src/components/dashboard/predications.jsx
@@ -0,0 +1,16 @@
+import React  from "react";
+import { TrendingUp, CheckCircle, AlertTriangle} from 'lucide-React';
+
+const Predications = () => {
+    //ML Model and their performance 
+    const models = [
+        {
+            name : 'Delay Predicton', 
+            type : ' Random Forest',
+            accuracy: 'Random Forest',
+            color : 'blue'
+        },
+},
+{
+    name :
+},
